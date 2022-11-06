31cfd45e647dc25c51129a572f23a71777ae4e22
Kwshro <112804380+Kwshro@users.noreply.github.com>
2022-11-06 11:14:57 +0100

    improved burger

--- site/menu.js
+++ site/menu.js
@@ -4,6 +4,11 @@ const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
+/* if menu class list contains show menu, it will remove show menu - it will display it as a block instead of an element
+then it will ad showmenu if it didn't contain it and swap the close icon and menu icon display block/none states
+it adds eventlistener to the hamburger menu on a click and goggles the function toggleMenu
+it will cycles through for each */
+
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
@@ -18,3 +23,8 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
