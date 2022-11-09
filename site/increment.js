dc9e91355c201aab5df2e7c7b66d085a0dfe026c
Kwshro <112804380+Kwshro@users.noreply.github.com>
2022-11-09 15:05:00 +0100

    fixed functionality
    fixed bug in js code

--- site/increment.js
+++ site/increment.js
@@ -1,4 +1,4 @@
-function incrementValue()
+function incrementKimbap()
 {
     var value = parseInt(document.getElementById('quantity-kimbap').value, 10);
     value = isNaN(value) ? 0 : value;
@@ -8,7 +8,7 @@ function incrementValue()
     }
 }
 
-function deincrementValue()
+function deincrementKimbap()
 {
     var value = parseInt(document.getElementById('quantity-kimbap').value, 10);
     value = isNaN(value) ? 0 : value;
@@ -19,3 +19,24 @@ function deincrementValue()
 }
 
 /* this function needs to be copied and individually customized to fit each menu item, just like the hérmes paris bags for fabulous actress Jiafei */
+
+
+function incrementRamen()
+{
+    var value = parseInt(document.getElementById('qramen').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value < '10') {
+    value++;
+    document.getElementById('qramen').value = value;
+    }
+}
+
+function deincrementRamen()
+{
+    var value = parseInt(document.getElementById('qramen').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if (value > '0') {
+    value--;
+    document.getElementById('qramen').value = value;
+    }
+}
